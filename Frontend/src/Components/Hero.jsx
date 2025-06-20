eccd4ba268ee33471160bb5da8811ab635ef7eca
Altaf Khan <altafpathan1k1@gmail.com>
2025-06-20 19:29:47 +0530

    ,,

--- Frontend/src/Components/Hero.jsx
+++ Frontend/src/Components/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
         <div className='w-full md:w-[40%] md:pr-[4rem] flex flex-col justify-center items-center md:items-start gap-2'>
             <h1 className='text-4xl md:text-5xl text-[#594a47] font-bold mb-2'>Descover Your Next Great Read</h1>
             <p className='mb-2 md:text-xl text-[18px] '>Uncover captivating stories, enriching knowledge, and endless inspiration in our curated collection of books</p>
-          <Link to={'/all-books'}> <Button variant="primary" className="px-6 text-xl"> Discover Books </Button></Link> 
+          <Link to={'/all-books'}> <Button variant="primary" className="px-6 text-xl"> Descover Books </Button></Link> 
         </div>
         <div className='w-full md:w-[58%]'>
             <img src="/Banner.jpg" alt="Bnner img" />
